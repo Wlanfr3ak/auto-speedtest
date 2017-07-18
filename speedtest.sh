97860c7ff379227ee4544ca7afc5bf5204a4a946
Fabian Horst <fabian@fabian-horst.de>
2017-07-18 06:13:31 +0200

    Update Path to Speedtest-Cli
    Added Codeline for changing from binary to the packetmanager installed version

--- speedtest.sh
+++ speedtest.sh
@@ -1,4 +1,7 @@
 #/bin/bash
+#When you have Installed the speedtest-cli via apt use this code
+#vf=$(speedtest --simple --server 4617)
+#When you have use the binary like the Readme Discribe use this line of Code
 vf=$(./speedtest-cli --simple --server 4617)
 ip=$(wget http://checkip.dyndns.org/ -q -O - | grep -Eo '\<[[:digit:]]{1,3}(\.[[:digit:]]{1,3}){3}\>')
 echo $ip
