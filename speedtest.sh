31adbfc90e23308800748947befc9ad91174856b
Bechir Zalila <bechir.zalila@gmail.com>
2019-02-23 13:45:52 +0100

    * Make script more robust

--- speedtest.sh
+++ speedtest.sh
@@ -2,15 +2,19 @@
 #When you have Installed the speedtest-cli via apt use this code
 #vf=$(speedtest --simple --server 4617)
 #When you have use the binary like the Readme Discribe use this line of Code
-vf=$(./speedtest-cli --simple --server 4617)
+work_dir="`pwd`"
+script_dir="`dirname $0`"; cd "${script_dir}"; script_dir="`pwd`"
+cd "${work_dir}"
+
+vf=$(speedtest-cli --simple --server 21506)
 ip=$(wget http://checkip.dyndns.org/ -q -O - | grep -Eo '\<[[:digit:]]{1,3}(\.[[:digit:]]{1,3}){3}\>')
 echo $ip
 DATE=$(date +"%Y-%m-%d ")
 TIME=$(date +"%H:%M:%S ")
 IN=$(echo $vf)
 SPEED=$(echo $IN | tr "[:alpha:]+[/:]" "\n")
-OUT=$DATE$TIME$SPEED
-vf2=$(echo $OUT | tr ' ' ';')
-echo "$vf2" >> speedtestausgabe-$ip.csv
+OUT=${DATE}${TIME}${SPEED}
+vf2=$(echo ${OUT} | tr ' ' ';')
+echo "${vf2}" >> "${script_dir}/speedtestausgabe-${ip}.csv"
 #DisplayOutput
 #python3 display.py $OUT
