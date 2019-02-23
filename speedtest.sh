31b301a823cef4a4687252008daf15d1b2f2f1d1
Bechir Zalila <bechir.zalila@gmail.com>
2019-02-23 14:22:17 +0100

    * Add date to file name 	* When no connection assume infinite ping and nul speed

--- speedtest.sh
+++ speedtest.sh
@@ -6,15 +6,20 @@ work_dir="`pwd`"
 script_dir="`dirname $0`"; cd "${script_dir}"; script_dir="`pwd`"
 cd "${work_dir}"
 
+# When no connection, this is practically equivalent to an infinite ping 
+# and nul speeds
+no_connection="100000 0.0 0.0"
+
 vf=$(speedtest-cli --simple --server 21506)
-ip=$(wget http://checkip.dyndns.org/ -q -O - | grep -Eo '\<[[:digit:]]{1,3}(\.[[:digit:]]{1,3}){3}\>')
+#ip=$(wget http://checkip.dyndns.org/ -q -O - | grep -Eo '\<[[:digit:]]{1,3}(\.[[:digit:]]{1,3}){3}\>')
 echo $ip
-DATE=$(date +"%Y-%m-%d ")
-TIME=$(date +"%H:%M:%S ")
+DATE=$(date +"%Y-%m-%d")
+TIME=$(date +"%H:%M:%S")
 IN=$(echo $vf)
 SPEED=$(echo $IN | tr "[:alpha:]+[/:]" "\n")
-OUT=${DATE}${TIME}${SPEED}
+[ -z "${SPEED}" ] && SPEED="${no_connection}"
+OUT="${DATE} ${TIME} ${SPEED}"
 vf2=$(echo ${OUT} | tr ' ' ';')
-echo "${vf2}" >> "${script_dir}/speedtestausgabe-${ip}.csv"
-#DisplayOutput
-#python3 display.py $OUT
+echo "${vf2}" >> "${script_dir}/speedtest-${DATE}.csv"
+#DisplayOutpum
+#mython3 display.py $OUT
